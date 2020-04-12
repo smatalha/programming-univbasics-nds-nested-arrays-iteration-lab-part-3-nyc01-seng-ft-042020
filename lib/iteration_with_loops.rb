751be20b1651eb837a3d39cf28c7863708acdbc4
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-12 15:03:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
       element_index = 0
       
       while element_index < src[row_index].length do
-        if  src[row_index][element_index].class == !Integer
+        if  src[row_index][element_index].class == String
             strings += src[row_index][element_index] + ' '
           
       end
